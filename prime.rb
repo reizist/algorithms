30c172b506086acd780a9148d869617b09429276
reizist <reizist@gmail.com>
2014-04-21 00:42:26 +0900

    素数カウントアルゴリズム追加

---
+++ prime.rb
@@ -0,0 +1,48 @@
+class Prime
+end
+
+def count_prime(max)
+  #配列作成
+  array = Array.new
+  #配列初期化
+  (0..max).each do |i|
+    array[i] = 1
+  end
+  #0,1は素数じゃない,2は素数
+  array[0],array[1],array[2] = 0,0,1
+
+  #3から最大値まで走査
+  (3..max).each do |i|
+    #奇数のみ素数対象
+    if i % 2 != 0
+      if array[i] != 0
+        (i+1..max).each do |j|
+          if i % 2 == 0
+            next
+          end
+          #既に走査した値の倍数を対象外に
+          if j % i == 0
+            array[j] = 0
+          end
+        end
+      end
+    else
+      array[i] = 0
+    end
+  end
+
+  #素数の個数をカウント
+  count = 0
+  (0...array.length).each do |i|
+    if array[i] == 1
+      print i.to_s + " "
+      count += 1
+    end
+  end
+  print "\n"
+  return count
+end
+
+print "最大値（整数): "
+max = STDIN.gets.chomp.to_i
+p count_prime(max)
